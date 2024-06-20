907f387ddc20d32e6ede4cee43d3a7afda406822
Alex Bowden <alex.bowden@Alexs-MacBook-Air.local>
2024-06-20 17:59:04 -0600

    populate image when enterd on contact form

--- src/Forms/NewContactForm.tsx
+++ src/Forms/NewContactForm.tsx
@@ -11,82 +11,98 @@ interface Props {
 }
 
 function NewContactForm({userId, categoryId, setDisplayNewContactForm, setContactIdForDetails, setUpdateContact}: Props) {
+    const [photoUrl, setPhotoUrl] = useState<string | undefined>(undefined)
     const postUrl = `${backendUrl}/users/${userId}/category/${categoryId}/contacts`
+
+    const defaultImageUrl = 'https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
     return (
         <>
-            <div className="card test-card shadow">
-                <form
-                    name="newContactForm"
-                    onSubmit={(e) => handleSubmit(e, postUrl, setDisplayNewContactForm, setContactIdForDetails, setUpdateContact)}
-                >
-                    <div className="row justify-content-between py-3">
-                        <div className="col">
-                            <i className="bi-file-person-fill h1 p-3 text-secondary"/>
-                        </div>
-                        <div className="col align-content-start text-end">
-                            <button
-                                className="btn"
-                                type="button"
-                                onClick={() => setDisplayNewContactForm(false)}
-                                >
-                                <i className="bi bi-x-lg"/>
-                            </button>
-                        </div>
+            <div className="card shadow">
+                <div className="row card-body">
+                    <div className="col">
+                        <img
+                            src={photoUrl ? photoUrl: defaultImageUrl}
+                            alt="profile-sample1"
+                            className="card-img img-square-card-lg shadow"
+                        />
                     </div>
-                    <div className="row g-2">
-                        <div className="col m-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Name"
-                                aria-label="Name"
-                                name="name"
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="row g-2">
-                        <div className="col-auto m-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Email"
-                                aria-label="Email"
-                                name="email"
-                            />
-                        </div>
-                        <div className="col-auto m-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Phone Number"
-                                aria-label="Phone Number"
-                                name="phoneNumber"
-                            />
-                        </div>
-                    </div>
-                    <div className="row g-2">
-                        <div className="col m-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Photo Url"
-                                aria-label="Photo Url"
-                                name="photoUrl"
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className="row justify-content-end m-2">
-                        <button
-                            className="btn btn-outline-secondary"
-                            type="submit"
+                    <div className="col-8">
+                        <form
+                            name="newContactForm"
+                            onSubmit={(e) => handleSubmit(e, postUrl, setDisplayNewContactForm, setContactIdForDetails, setUpdateContact)}
                         >
-                            <i className="bi bi-rocket-takeoff"> Submit </i>
-                        </button>
+                            <div className="row justify-content-between py-3">
+                                <div className="col">
+                                    <i className="bi-file-person-fill h1 p-3 text-secondary"/>
+                                </div>
+                                <div className="col align-content-start text-end">
+                                    <button
+                                        className="btn"
+                                        type="button"
+                                        onClick={() => setDisplayNewContactForm(false)}
+                                    >
+                                        <i className="bi bi-x-lg"/>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="row g-2">
+                                <div className="col m-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Photo Url"
+                                        aria-label="Photo Url"
+                                        name="photoUrl"
+                                        onInput={(e) => setPhotoUrl(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="row g-2">
+                                <div className="col m-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Name"
+                                        aria-label="Name"
+                                        name="name"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="row g-2">
+                                <div className="col-auto m-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Email"
+                                        aria-label="Email"
+                                        name="email"
+                                    />
+                                </div>
+                                <div className="col-auto m-2">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Phone Number"
+                                        aria-label="Phone Number"
+                                        name="phoneNumber"
+                                    />
+                                </div>
+                            </div>
+                            <div className="row justify-content-end m-2">
+                                <button
+                                    className="btn btn-outline-secondary"
+                                    type="submit"
+                                >
+                                    <i className="bi bi-rocket-takeoff"> Submit </i>
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                </form>
+
+                </div>
             </div>
         </>
     )
